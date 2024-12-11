6f4e04c583d31b97e25f957250cacceba8cbb228
Xuantm <truong.xuantm@gmail.com>
2024-12-11 16:00:43 +0700

    remove wordpress

--- apps/wp/push.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-git subtree push --prefix=wp-content/plugins/masterstudy-lms-learning-management-system https://github.com/GNH-Team/MasterStudy.git main &&
-    git subtree push --prefix=wp-content/plugins/masterstudy-lms-learning-management-system-pro https://github.com/GNH-Team/MasterStudy-Pro.git main
